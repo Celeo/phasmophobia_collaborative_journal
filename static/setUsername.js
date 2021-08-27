c0861f4013caaf5aeff3db0f1ca1518cc88327fe
Celeo <mattboulanger@fastmail.com>
2021-08-27 13:41:30 -0700

    Misc

--- static/setUsername.js
+++ static/setUsername.js
@@ -1,6 +1,5 @@
 const fromStorage = localStorage.getItem("name");
 if (fromStorage === null || fromStorage.length === 0) {
-  const randomName = window.chance.first();
   localStorage.setItem("pickedRandom", true);
-  localStorage.setItem("name", randomName);
+  localStorage.setItem("name", window.chance.first());
 }
